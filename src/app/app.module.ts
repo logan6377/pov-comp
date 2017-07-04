230e187bdbb59c9be4f4cb8569f9964e50776b07
Loganathan Arumugam (GC) <larumugam@triadretail.com>
2017-07-04 03:44:54 -0400

    Setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout.component';
+import { HeaderComponent } from './layout/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
