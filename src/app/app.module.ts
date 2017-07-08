81d5797df67d443a566878cdd7c8bbfbc357ba92
Loganathan Arumugam <loganathan.arumugam@wearefmg.net>
2017-07-08 17:16:56 +0530

    j

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout.component';
 import { HeaderComponent } from './layout/header.component';
-
+ 
 @NgModule({
   declarations: [
     AppComponent,
--- src/app/layout/header.component.ts
+++ src/app/layout/header.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
     <div class="header">
       <div class="row">
         <div class="col-md-2"><a href="/"><img src="../../assets/images/logo.png"></a></div>
-        <div class="col-md-2 pull-right aligh-right text-right"><a href="/">larumugam@triadretail.com <span class="glyphicon glyphicon-chevron-down"></span></a></div>
+        <div class="col-md-2 pull-right aligh-right text-right"><a href="/">alarumugam@triadretail.com <span class="glyphicon glyphicon-chevron-down"></span></a></div>
       </div>
     </div>
   `,
